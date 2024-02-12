5bfa85488ac8ad612df1c9b9e3269c3151c58ea5
Blake Hufford <119457623+Nam3lessKNG@users.noreply.github.com>
2024-02-12 12:02:03 -0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+// C++ program to display "Hello World"
 
-int main{
-  std::cout << "Hello World!";
-  return 0;
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+// Main() function: where the execution of
+// program begins
+int main()
+{
+	// Prints hello world
+	cout << "Hello World";
+
+	return 0;
 }
